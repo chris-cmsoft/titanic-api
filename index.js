ac71ad742f9d7b88b4917adc535ca2eda575a235
Christiaan Vermeulen <chrism.vermeulen@gmail.com>
2022-01-27 09:56:30 +0100

    Separate server and routes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   return res.json({
@@ -9,6 +8,4 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Titanic API listening on port ${port}`);
-});
+exports = module.exports = app
